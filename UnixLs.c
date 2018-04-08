2ecc682adb3c871a62fa5b7f0c4f21f4c1f5e7f5
Junha Choi <junhac@sfu.ca>
2018-04-08 16:05:13 -0700

    changed functions' arugments structure such that UnixLs's mode can be easily set

--- UnixLs.c
+++ UnixLs.c
@@ -10,28 +10,35 @@
 
 const char *months[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
-void printFile_Normal(struct dirent *entry);
-void printFile_Long(struct dirent *entry);
-void printDir_R(const char *path, void (*f)(struct dirent*));
+void printFile_Normal(struct dirent *entry, int i);
+void printFile_Long(struct dirent *entry, int i);
+void printDir(const char *path, void (*f)(struct dirent*, int), int i, int R);
 
 void transferModeToString(struct stat);
 
 int main( int argc, char **argv){
+    int l = 1;
     if(argc == 1){
-        printDir_R(".", printFile_Long);
+        if(l == 0)
+            printDir(".", printFile_Normal, 0, 0);
+        else if(l == 1)
+            printDir(".", printFile_Long, 0, 0);
         printf("\n");
         return 0;
     }
+    
     printf("num argument: %d\n", argc);
     
     return 0;
 }
 
-void printFile_Normal(struct dirent *entry){
+void printFile_Normal(struct dirent *entry, int i){
+    if(i==1)
+        printf("%1lld  ", entry->d_ino);
     printf("%s\t", entry->d_name);
 }
 
-void printFile_Long(struct dirent *entry){
+void printFile_Long(struct dirent *entry, int i){
     char buf[1024];
     struct stat fileStat;
     struct passwd *pw = NULL;
@@ -42,8 +49,8 @@ void printFile_Long(struct dirent *entry){
     grp = getgrgid(fileStat.st_gid); 
     pw = getpwuid(fileStat.st_uid);
     time = gmtime(&fileStat.st_mtime);
-
-    printf("%11llu", fileStat.st_ino);
+    if(i==1)
+        printf("%11llu", fileStat.st_ino);
     transferModeToString(fileStat);
     printf("%4d", fileStat.st_nlink);
     printf("%15s", pw->pw_name);
@@ -58,7 +65,7 @@ void printFile_Long(struct dirent *entry){
     printf("\n");
 }
 
-void printDir_R(const char *path, void (*printFile)(struct dirent*)) {
+void printDir(const char *path, void (*printFile)(struct dirent*, int), int i, int R) {
     DIR *dir;
     struct dirent *entry;
 
@@ -67,25 +74,30 @@ void printDir_R(const char *path, void (*printFile)(struct dirent*)) {
 
     // print current dir's files
     while ((entry = readdir(dir)) != NULL){
-        if(entry->d_type == DT_REG || entry->d_type == DT_DIR || entry->d_type == DT_LNK)
-            printFile(entry);
+        if(entry->d_type == DT_REG || entry->d_type == DT_DIR || entry->d_type == DT_LNK){
+            if( strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+                continue;
+            printFile(entry, i);
+        }
     }
     closedir(dir);
 
     // print subdir's files
-    if(!(dir = opendir(path)))
-        return;
-    while ((entry = readdir(dir)) != NULL){
-        if (entry->d_type == DT_DIR) {
-            char nextPath[1024];
-            if( strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
-                 continue;
-            snprintf(nextPath, sizeof(nextPath), "%s/%s", path, entry->d_name);
-            printf("\n\n%s:\n", nextPath);
-            printDir_R(nextPath, printFile);
-        }
-    }   
-    closedir(dir);
+    if (R == 1){
+        if(!(dir = opendir(path)))
+            return;
+        while ((entry = readdir(dir)) != NULL){
+            if (entry->d_type == DT_DIR) {
+                char nextPath[1024];
+                if( strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+                    continue;
+                snprintf(nextPath, sizeof(nextPath), "%s/%s", path, entry->d_name);
+                printf("\n\n%s:\n", nextPath);
+                printDir(nextPath, printFile, i, R);
+            }
+        }   
+        closedir(dir);
+    }
 }
 
 void transferModeToString( struct stat fileStat){
