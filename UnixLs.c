6accacd279d10f3ed2f8656189dec5248b013bf8
Junha Choi <junhac@sfu.ca>
2018-04-08 12:03:43 -0700

    implemented -R option for UnixLs

--- UnixLs.c
+++ UnixLs.c
@@ -4,14 +4,50 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-void printDir(const char *path, int indent);
+void printFile_Normal(struct dirent *entry);
+void printDir_R(const char *path, void (*f)(struct dirent*));
 
 int main( int argc, char **argv){
     if(argc == 1){
-        printf("UnixLS needs one path input");
-        return 1;
+        printDir_R(".", printFile_Normal);
+        printf("\n");
+        return 0;
     }
     printf("num argument: %d\n", argc);
     
     return 0;
+}
+
+void printFile_Normal(struct dirent *entry){
+    printf("%s\t", entry->d_name);
+}
+
+void printDir_R(const char *path, void (*printFile)(struct dirent*)) {
+    DIR *dir;
+    struct dirent *entry;
+
+    if(!(dir = opendir(path)))
+        return;
+
+    // print current dir's files
+    while ((entry = readdir(dir)) != NULL){
+        if(entry->d_type == DT_REG || entry->d_type == DT_DIR)
+            printFile(entry);
+    }
+    closedir(dir);
+
+    // print subdir's files
+    if(!(dir = opendir(path)))
+        return;
+    while ((entry = readdir(dir)) != NULL){
+        if (entry->d_type == DT_DIR) {
+            char nextPath[1024];
+            if( strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+                 continue;
+            snprintf(nextPath, sizeof(nextPath), "%s/%s", path, entry->d_name);
+            printf("\n\n%s:\n", nextPath);
+            printDir_R(nextPath, printFile);
+        }
+    }   
+    closedir(dir);
 }
